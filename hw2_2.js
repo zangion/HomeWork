6053b1eafc9b0a19caf9ee3254c90d8d84f3a61c
Ваше имя < >
2023-06-01 11:33:17 +0300

    completed js tasks

---
+++ hw2_2.js
@@ -0,0 +1,93 @@
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// Задание 1
+
+const iphoneYear = 2007;
+alert(iphoneYear);
+
+// Задание 2
+
+const jsCreator = "Brendan Eich";
+alert(jsCreator);
+
+// Задание 3
+
+let b = 10;
+let c = 2;
+alert(b + c);
+alert(b - c);
+alert(b * c);
+alert(b / c);
+
+// Задание 4
+
+let result = 2 ** 5;
+alert(result);
+
+// Задание 5
+
+let a1 = 9;
+let b1 = 2;
+let devision = a1 % b1;
+alert(devision);
+
+// Задание 6
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+// Задание 7
+
+let age = prompt("Сколько вам лет?");
+alert(age);
+
+// Задание 8
+
+let user = {
+    name: 'Bob',
+    age: 32,
+    isAdmin: true
+};
+
+// Задание 9
+
+user[`city of residence`] = `Vladikavkaz`;
+
+// Задание 9.1
+
+user[age] = 25;
+console.log(user);
+
+// Задание 9.2
+
+delete user[ `city of residence` ];
+
+// Задание 9.3
+
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+console.log(user[info]);
+alert(user[info]);
+
+// Задание 9.4
+
+let name = prompt('Ваше имя?');
+alert(`Привет, ${name}`);
+
+// Задание 10.
+
+
+
+
+
+
+
+
